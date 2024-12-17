f760ad04ccba6efdf2ac8ef1721e1e59a57dd754
Manjeet Roy <ymanjeet7063@gmail.com>
2024-12-17 20:51:10 +0530

    Error resolved

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
     };
 
     //create token
-    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
       expiresIn: "1d",
     });
 
